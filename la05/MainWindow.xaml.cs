a0476a48359fdab31a92a1dcb7b6270b323712a2
Bence <fbencef@stud.uni-obuda.hu>
2023-04-04 11:41:51 +0200

    Added Load button functionality

--- la05/MainWindow.xaml.cs
+++ la05/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,18 @@ namespace la05
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Sportsman> sportsmen = new ObservableCollection<Sportsman>();
         public MainWindow()
         {
             InitializeComponent();
+            listBox_sportsmen.ItemsSource = sportsmen;
+        }
+
+        private void button_load_Click(object sender, RoutedEventArgs e)
+        {
+            sportsmen.Add(new Sportsman("Jack", 75, 70, true, "Club1", 50));
+            sportsmen.Add(new Sportsman("Jack", 70, 75, false, "Club1", 50));
+            sportsmen.Add(new Sportsman("James", 78, 40, true, "Club3", 52));
         }
     }
 }
