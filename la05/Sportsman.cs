9ed62204e50f7c33a50d44a041f65c65fd20c617
Bence <fbencef@stud.uni-obuda.hu>
2023-04-04 11:40:41 +0200

    Added class Sportsman

---
+++ la05/Sportsman.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace la05
+{
+    public class Sportsman
+    {
+        private string name;
+        private int personalBest;
+        private int annualBest;
+        private bool hasPermit;
+        private string club;
+        private int number;
+
+        public Sportsman(string name, int personalBest, int annualBest, bool hasPermit, string club, int number)
+        {
+            this.name = name;
+            this.personalBest = personalBest;
+            this.annualBest = annualBest;
+            this.hasPermit = hasPermit;
+            this.club = club;
+            this.number = number;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int PersonalBest { get => personalBest; set => personalBest = value; }
+        public int AnnualBest { get => annualBest; set => annualBest = value; }
+        public bool HasPermit { get => hasPermit; set => hasPermit = value; }
+        public string Club { get => club; set => club = value; }
+        public int Number { get => number; set => number = value; }
+        public int MarketValue { get => personalBest * annualBest; }
+
+        
+    }
+}
