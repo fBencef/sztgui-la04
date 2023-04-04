ee9b290c04e69aae81eb662002d6c1632fbb0153
Bence <fbencef@stud.uni-obuda.hu>
2023-04-04 11:56:37 +0200

    Permit background implemented

---
+++ la05/PermitToBackgroundConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace la05
+{
+    public class PermitToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool hasPermit = (bool)value;
+            Brush background = Brushes.LightGreen;
+
+            if (!hasPermit) background = Brushes.LightSalmon;
+
+            return background;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
